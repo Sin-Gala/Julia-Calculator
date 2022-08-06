49e38dea2e9abd2afdb89b4d524190539c4dd335
SinGala67 <galahane.rouille@gmail.com>
2022-08-06 21:27:55 +0200

    Update Calculator.jl

--- src/Calculator.jl
+++ src/Calculator.jl
@@ -1,9 +1,31 @@
+clear() = run(`cmd /c cls`)
+
+# Basic calculator in console test
 currentNumber = 0
 
-function add(numbers)
-    for n in numbers 
-        currentNumber += n
+function waitingForInput()
+    clear()
+    println("Please enter a number \n")
+    println("Enter blank to end this program\n")
+
+    command = readline()
+
+    if (isempty(command))
+        exit()
+    elseif (tryparse(Int, command) !== nothing)
+        add(parse(Int, command))
+    else
+        waitingForInput()
     end
+end
+
+#region CALCULUS
+function add(number)
+    currentNumber += number
+
+    println("Current Number: " + currentNumber)
+
+    waitingForInput()
 end
 
 function remove(numbers)
@@ -11,3 +33,6 @@ function remove(numbers)
         currentNumber -= n
     end
 end
+#endregion
+
+waitingForInput()
